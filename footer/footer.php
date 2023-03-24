7dd7656cb6899659e05f4733689e0bb06edc9ba9
phelipe009 <126103050+phelipe009@users.noreply.github.com>
2023-03-23 23:20:57 -0500

    si

---
+++ footer/footer.php
@@ -0,0 +1,22 @@
+            <footer>
+                <section class="izqpp">
+                    <h3>Contacto:</h3>
+                    <h4>Tel: 672 08 93</h4>
+                    <h4>Correo: drogueríala20@gmail.com</h4>
+                    <h4>Facebook: Drogueríala20</h4>
+                    <h4>Instagram: @Drogueríala20</h4>
+                    
+                </section>
+                <section class="cenpp">
+                    <h3>
+                        Dirección:
+                    </h3>
+                    <h4> Quibdó - Chocó </h4>
+                    <h4> Cl 20 #4-04 </h4>
+                </section>
+                <section class="derpp">
+                    <h4>© 2021</h4>
+                    <h4>By @Andrés Marín @Eligio @Phelipe</h4>
+                    <h4>Producto creado con VSCode</h4>
+                </section>
+            </footer>
---
+++ functions/bd_conn.php
@@ -0,0 +1,7 @@
+<?php 
+    $conn = mysqli_connect('127.0.0.1:33065', 'root', '', 'drogeria');
+    if(!$conn){
+        echo 'Error:';
+    }
+?>
+
